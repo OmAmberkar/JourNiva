7afb0e1fc88fcd74acab0f0933241824c2617601
“Nitin-2809” <nitinmirajkar2@gmail.com>
2025-06-22 13:53:16 +0530

    added DaisyUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
+import './App.css'; // Assuming you have a global CSS file
 const App = () => {
   return (
     <div>
