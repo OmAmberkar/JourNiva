cf75ae73b1cf59a62f477c1af17ef3479bf23c22
ishaanpgujaran <ishaangujaran@gmail.com>
2025-06-25 01:07:01 +0530

    Created basic structure of user.controller.js : Route Controller 5,6,7,8 Updated imports of user.routes.js

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -35,3 +35,35 @@ export const sendOTP = (req, res) => {
 
     res.status(200).json(message = "OTP Sent Successfully to your Email!");
 };
+
+
+//Route 5 Controller - Register : Email OTP Verification
+export const verifyOTP = (req, res) => {
+    const { otp } = req.body;
+
+    res.status(200).json({ message: "Email Verified Successfully!"});
+};
+
+
+//Route 6 Controller - Forget Password : Link Generation
+export const forgetPasswordLink = (req, res) => {
+    const { email } = req.body;
+    
+    res.status(200).json({ message: "Password Reset Link has been Sent to your Registered Email!"});
+};
+
+
+//Route 7 Controller - Forget Password : Validate Token
+export const validateToken = (req, res) => {
+    const { token } = req.params;
+
+    res.status(200).json({ message: "Token is Valid!"});
+};
+
+
+//Route 8 Controller - Forget Password : Reset Password
+export const resetPassword = (req, res) => {
+    const { password } = req.body;
+
+    res.status(200).json({ message: "Password has been Reset Successfully!"});
+};
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,52 +1,29 @@
 import express from "express"
-import { checkEmail, registerUser, loginUser, sendOTP } from "../controllers/user.controller.js" 
+import { checkEmail, registerUser, loginUser, sendOTP, verifyOTP, forgetPasswordLink, validateToken, resetPassword } from "../controllers/user.controller.js" 
 const router = express.Router();
 
 //Route 1 - Check Email
 router.post("/check-email" , checkEmail);
 
-
 //Route 2 - Register : Create New User & Store details
 router.post("/register" , registerUser);
-
 
 //Route 3 - Login Existing User
 router.post("/login" , loginUser);
 
-
 //Route 4 - Register : Email OTP Generation & Send
 router.post("/send-otp" , sendOTP);
 
-
 //Route 5 - Register : Email OTP Verification
-router.post("verify-otp" , (req, res) => {
-    const { otp } = req.body;
-
-    res.status(200).json({ message: "Email Verified Successfully!"});
-});
-
+router.post("verify-otp" , verifyOTP);
 
 //Route 6 - Forget Password : Link Generation
-router.post("/forget-password-link" , (req, res) => {
-    const { email } = req.body;
-    
-    res.status(200).json({ message: "Password Reset Link has been Sent to your Registered Email!"});
-});
-
+router.post("/forget-password-link" , forgetPasswordLink);
 
 //Route 7 - Forget Password : Validate Token
-router.get("validate-token/:token" , (req, res) => {
-    const { token } = req.params;
-
-    res.status(200).json({ message: "Token is Valid!"});
-});
-
+router.get("validate-token/:token" , validateToken);
 
 //Route 8 - Forget Password : Reset Password
-router.post("/reset-password" , (req, res) => {
-    const { password } = req.body;
-
-    res.status(200).json({ message: "Password has been Reset Successfully!"});
-});
+router.post("/reset-password" , resetPassword);
 
 export default router
