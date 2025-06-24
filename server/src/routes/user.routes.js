78144f8270d77e03aa080e29212ba262623c3c06
ishaanpgujaran <ishaangujaran@gmail.com>
2025-06-25 00:21:37 +0530

    Created Basic Structure of user.routes.js : Route 5,6,7,8

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -11,7 +11,8 @@ router.post("/check-email" , (req, res) => {
      } else {
         res.status(200).json({message : "New User"});
      }
-})
+});
+
 
 //Route 2 - Register : Create New User & Store details
 router.post("/register" , (req, res) => {
@@ -21,20 +22,54 @@ router.post("/register" , (req, res) => {
     const { avatarUrl } = req.body.avatarUrl ;
 
     res.status(201).json({message : "User Registered Successfully"});
-})
+});
+
 
 //Route 3 - Login Existing User
 router.post("/login" , (req, res) => {
     const { password } = req.body ;
 
     res.status(200).json({message : "User Logged in Successfully"}) ;
-})
+});
 
 
-//Route 4 - Register : Email OTP Verification
+//Route 4 - Register : Email OTP Generation & Send
 router.post("/send-otp" , (req, res) => {
     const { email } = req.body ;
 
     res.status(200).json(message = "OTP Sent Successfully to your Email!");
-})
+});
+
+
+//Route 5 - Register : Email OTP Verification
+router.post("verify-otp" , (req, res) => {
+    const { otp } = req.body;
+
+    res.status(200).json({ message: "Email Verified Successfully!"});
+});
+
+
+//Route 6 - Forget Password : Link Generation
+router.post("/forget-password-link" , (req, res) => {
+    const { email } = req.body;
+    
+    res.status(200).json({ message: "Password Reset Link has been Sent to your Registered Email!"});
+});
+
+
+//Route 7 - Forget Password : Validate Token
+router.get("validate-token/:token" , (req, res) => {
+    const { token } = req.params;
+
+    res.status(200).json({ message: "Token is Valid!"});
+});
+
+
+//Route 8 - Forget Password : Reset Password
+router.post("/reset-password" , (req, res) => {
+    const { password } = req.body;
+
+    res.status(200).json({ message: "Password has been Reset Successfully!"});
+});
+
 export default router
