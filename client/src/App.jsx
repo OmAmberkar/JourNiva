2bd7ae50bf26a162bd20b6f565e1ee73f7d21908
“Nitin-2809” <nitinmirajkar2@gmail.com>
2025-06-29 18:05:35 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,8 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return <div>
+    <Header />
     
-    <Footer />
   </div>
 };
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,12 +7,12 @@ export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-[#DCEFFF] font-sans">
+    <div className="min-h-screen bg-[#DCEFFF] font-Livvic">
       {/* Navbar */}
       <nav className="w-full flex items-center justify-between px-4 md:px-6 py-2 bg-[#DCEFFF] border-b-2 border-[#3E5973] relative">
         <div className="flex items-center space-x-2">
           <img src={Logo} alt="logo" className="h-14 w-14 object-contain" />
-          <h1 className="text-[#3E5973] text-2xl md:text-3xl font-semibold">
+          <h1 className="text-[#3E5973] text-2xl md:text-3xl ">
             JourNiva
           </h1>
         </div>
