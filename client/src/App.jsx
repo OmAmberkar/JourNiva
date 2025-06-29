a96d296ec93918910e19ca674163b450b541fa5f
Disha Chaudhari <dishachaudhari801@gmail.com>
2025-06-29 15:25:25 +0530

    new start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,7 @@
-import { Routes } from "react-router";
-import LandingPage from "./pages/LandingPage";
-import RegisterPage from "./pages/RegisterPage";
-import LoginPage from "./pages/LoginPage";
+import React from "react";
 
-import "./App.css"; // Assuming you have a global CSS file
 const App = () => {
-  return (
-    <div>
-      {/* <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes> */}
-      <LoginPage />
-    </div>
-  );
+  return <div>hello</div>;
 };
 
 export default App;
