00604967962d03d7e6f00cc472c0da1ced8437f6
“Nitin-2809” <nitinmirajkar2@gmail.com>
2025-06-29 16:00:09 +0530

    Page Creations

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>hello</div>;
+  return <div>
+    <Header />
+  </div>
 };
 
 export default App;
