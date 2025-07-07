9b6b5ce2c521fdc85c665b0572fd4acf3c6e7254
ishaanpgujaran <ishaangujaran@gmail.com>
2025-07-07 22:08:37 +0530

    Updated user Route 1 checkEmail controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,12 +1,32 @@
 // Route 1 Controller - Check Email
- export const checkEmail = (req, res) => {
+ export const checkEmail = async (req, res) => {
     const { email } = req.body ;
+    if (!email) {
+        return res.status(400).json({ message: "Email is required!" });
+    }
 
-    if (email) {
-        res.status(200).json({ message : "Existing User"});
-     } else {
-        res.status(200).json({message : "New User"});
-     }
+    try {
+        const existingUser = await Users.findOne({ email });
+        if (existingUser) {
+            res.status(200).json({ 
+                status : 1,
+                message : "Existing User",
+                user : {
+                    name : existingUser.name,
+                    avatarUrl : existingUser.avatarUrl
+                }
+            });
+        } else {
+            return res.status(200).json({ 
+                status: 0,
+                message: "New User",
+            });
+        }
+    } catch (error) {
+        console.log("Error in Checking Email :".error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+
 };
 
 
