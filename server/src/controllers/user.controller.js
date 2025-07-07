63d29f2c4c329dd92f25b802d85074c0195aa1c6
shivanissingh <shivanissingh01@gmail.com>
2025-07-07 22:00:54 +0530

    Created User Route 1 : check-email Controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,12 +1,30 @@
 // Route 1 Controller - Check Email
- export const checkEmail = (req, res) => {
+ export const checkEmail = async (req, res) => {
     const { email } = req.body ;
+    
+    try{
+        const existingUser = await Users.findOne({ email }) ;
+        if (existingUser) {
+            res.status(200).json({
+                status : 1,
+                message : "Existing User",
+                user : {
+                    id : existingUser._id,
+                    name : existingUser.name,
+                    avatarUrl : existingUser.avatarUrl
+                }
+            });
+        } else {
+            res.status(200).json({
+                status : 0,
+                message : "New User" 
+            });
+        };
+    } catch (error) {
+        console.error("Error Checking Email :", error);
+        res.status(500).json({message : "Internal Server Error"});
+    }; 
 
-    if (email) {
-        res.status(200).json({ message : "Existing User"});
-     } else {
-        res.status(200).json({message : "New User"});
-     }
 };
 
 
