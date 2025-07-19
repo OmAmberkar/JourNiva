54425bb116b0bafb8856835d5d44ffc432a5272c
Disha Chaudhari <dishachaudhari801@gmail.com>
2025-07-19 20:37:56 +0530

    few changes made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import ScrollToTop from "./components/ScrollToTop"; 
+import ScrollToTop from "./components/ScrollToTop";
 import SignIn from "./components/SignIn";
 import AboutUsPage from "./Pages/AboutUsPage";
 import LandingPage from "./Pages/LandingPage";
 import GetStarted from "./Pages/GetStarted";
 import SignUp from "./components/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutUsPage />} />
           <Route path="/getstarted" element={<GetStarted />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           {/* <Route path="/" element={<SignIn />} />  */}
           {/* <Route path="/" element={<SignUp />} /> */}
         </Routes>
---
+++ client/src/Pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LeftSideBar from "../components/LeftSideBar";
+import RightSideBar from "../components/RightSideBar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard
+      <LeftSideBar />
+      <RightSideBar />
+    </div>
+  );
+};
+
+export default Dashboard;
