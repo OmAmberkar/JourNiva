9778b251ecc16328c5cfd95e13ac3e230887fc93
shivanissingh <shivanissingh01@gmail.com>
2025-07-21 13:43:50 +0530

    Minor Update 2 - user.controller.js Error Fixed

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -103,16 +103,11 @@ export const loginUser = async (req, res) => {
     const temail = email.trim() ;
 
     //Validate Email & Password
-<<<<<<< HEAD
-    if (!email || !password) {
+    if (!temail || !password) {
         return res.status(400).json({ 
             status : "failed",
             message: "Email and Password are Required!" 
         }) ;
-=======
-    if (!temail || !password) {
-        return res.status(400).json({ message: "Email and Password are Required!" }) ;
->>>>>>> origin/main
     }
 
   
