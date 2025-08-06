9e955d1d00ec8c168895fdf1a8a76fa7d4b14b75
ishaanpgujaran <ishaangujaran@gmail.com>
2025-08-06 23:57:58 +0530

    Major Update 14 - Implementation of Remember Me Part I

--- server/src/utils/jwtUtils.js
+++ server/src/utils/jwtUtils.js
@@ -8,23 +8,25 @@ export const generateAccessToken = (userId) => {
 };
 
 //Function to Generate Refresh Token (Long Lived)
-export const generateRefreshToken = (userId) => {
+export const generateRefreshToken = (userId, rememberMe) => {
     return jwt.sign({ userId }, process.env.JWT_REFRESH_TOKEN_SECRET, { 
-        expiresIn: '7d' 
+        expiresIn: rememberMe ? '30d' : '3h'
     });
 };
 
 //Function to Call Generate Tokens & Send Cookie
-export const sendTokenResponse = (res, userId) => {
+export const sendTokenResponse = (res, userId, rememberMe = false) => {
     const accessToken = generateAccessToken(userId);
-    const refreshToken = generateRefreshToken(userId);
+    const refreshToken = generateRefreshToken(userId, rememberMe);
+
+    const cookieExpiry = rememberMe ? 30 * 24 * 60 * 60 * 1000 : 3 * 60 * 60 * 1000; // 30 Days or 3 Hours based on rememberMe
 
     // Set the refresh token in an HTTP-only cookie
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production', // Use secure cookies in production only
         sameSite: 'Lax',
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+        maxAge: cookieExpiry
     });
 
     //Return the Access Token for Passing to Frontend
